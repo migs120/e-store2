6230a81180f7622c023e1e1aad59dff1e87b4c43
Your Name <migs432@yahoo.com>
2016-10-06 18:29:32 +0000

    angular image load working and email notify working!!

--- app/assets/stylesheets/js/application/controllers/LandingCtrl.js
+++ app/assets/stylesheets/js/application/controllers/LandingCtrl.js
@@ -2,6 +2,8 @@
     function LandingCtrl($scope,Image) {
                                       
                                         $scope.image = Image ; 
+      
+                                        console.log("working")
                                     //  $Scope.image = return "https://s3-us-west-2.amazonaws.com/pics120/mario-e-store/main_categories/index/GnM.png"
                                          
                                         }
--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,5 +1,5 @@
 class Mailer < ActionMailer::Base
-  default from: "migs432@yahoo.com"
+  default from: "migs@miggymigs.com"
   def email(data)
     mail(to: "migs910@hotmail.com", subject: "testing email").deliver
   end
