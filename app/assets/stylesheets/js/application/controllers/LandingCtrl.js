d62616cb3b095d23fd485512eeacc7829208539c
Your Name <migs432@yahoo.com>
2016-11-09 16:03:45 +0000

    saving changes

--- app/assets/stylesheets/js/application/controllers/LandingCtrl.js
+++ app/assets/stylesheets/js/application/controllers/LandingCtrl.js
@@ -3,8 +3,6 @@
                                       
                                         $scope.image = Image ; 
       
-                                        console.log("working")
-                                    //  $Scope.image = return "https://s3-us-west-2.amazonaws.com/pics120/mario-e-store/main_categories/index/GnM.png"
                                          
                                         }
     
